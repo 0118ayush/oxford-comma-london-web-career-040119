e5bbfe59bbb094c9466d36475ec05f8c93b39c68
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-25 14:04:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   if array.length == 1 
-    array.join()
+    array.join(" ")
   elsif array.length == 2 
     array.join(" and ")
   elsif array.length > 2
     lastelement = array.pop 
     firstpart = array.join(", ")
-    endpart = lastelement.join(" and ")
+    endpart = lastelement.join("and ")
     firstpart + endpart
   end
 end
